053e6cd9051a84661018d10aa8471d2f59f9e18f
Andrew Carbungco <andrew.carbungco@gmail.com>
2020-11-18 23:51:53 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function toggle() {
   var x = document.getElementById("footer");
   var y = document.getElementById("toggleArrow");
+  var z = document.getElementById("doubletap");
   if (x.style.display === "none") {
     x.style.display = "grid";
     x.style.transitionDuration = "1s";
@@ -9,6 +10,7 @@ function toggle() {
   } else {
     x.style.display = "none";
     y.style.transform = "rotate(-135deg)";
+    z.style.display = "none";
   }
 }
 
